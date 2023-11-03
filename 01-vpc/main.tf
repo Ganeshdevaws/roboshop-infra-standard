25dc365d1a61ecb97fc9ad99317973df6a7e1d9b
ganesh <ganeshjanapareddy2019@gmail.com>
2023-11-03 23:53:25 +0530

    jenkins

--- 01-vpc/main.tf
+++ 01-vpc/main.tf
@@ -2,7 +2,6 @@ module "vpc" {
   source = "git::https://github.com/Ganeshdevaws/terraform-aws-vpc-advanced.git"
   project_name = var.project_name #"roboshop" instead of hard coding we are keeping in variables.tf
   cidr_block = var.cidr_block
-  env = var.env
   common_tags = var.common_tags
   public_subnet_cidr = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
